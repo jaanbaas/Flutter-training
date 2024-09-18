26086b8b89e8e9337577c570a0b2d29cb80d2936
jaanbaas <74250504+jaanbaas@users.noreply.github.com>
2024-09-18 23:50:33 +0530

    18-09-24

---
+++ login_page.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sample App",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w800),
+        ),
+        backgroundColor: const Color(0xff0393e7),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'TutorialKart',
+              style: TextStyle(
+                  color: Color(0xff049df4),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text('Sign in'),
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+              width: 300,
+              height: 40,
+              child: Container(
+                // width: 250,
+                // height: 60,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3), // Shadow position
+                    ),
+                  ],
+                  border: Border.all(),
+                ),
+                child: TextField(
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'User Name',
+                    hintStyle:
+                        const TextStyle(color: Colors.grey, fontSize: 16),
+                    // border: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(8)),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      // borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff049df4), // Border color when focused
+                        // Border width
+                      ),
+                      // borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 10.0),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 300,
+              height: 40,
+              child: Container(
+                // width: 250,
+                // height: 60,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3), // Shadow position
+                    ),
+                  ],
+                  border: Border.all(),
+                ),
+                child: TextField(
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle:
+                        const TextStyle(color: Colors.grey, fontSize: 16),
+                    // border: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(8)),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      // borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff049df4), // Border color when focused
+                        // Border width
+                      ),
+                      // borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 10.0),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Forgot password?',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xff049df4)),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  foregroundColor: Color(0xffffffff),
+                  backgroundColor: const Color(0xff049df4),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0))),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  // color: Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Don\'t have an account'),
+                TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        foregroundColor: const Color(0xff049df4),
+                        backgroundColor: const Color(0xffffffff)),
+                    child: const Text(
+                      'Sign up',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
